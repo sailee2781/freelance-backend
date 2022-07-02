fb5648af4f4edb1977e1b7119096372b9d5730d0
Sailee Kadam <sailee.kadam19@vit.edu>
2022-07-02 14:56:41 +0530

    modified db url

--- dbs/conn.js
+++ dbs/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const local = "mongodb://localhost:27017/freelance";
-const hosted = "mongodb+srv://freelance_services:freelance_services@cluster0.gyndj.mongodb.net/?retryWrites=true&w=majority";
+const hosted = "mongodb+srv://freelance_services:freelance_services@cluster0.gyndj.mongodb.net/freelance-db/?retryWrites=true&w=majority";
 
 mongoose.connect(hosted,{
     useNewUrlParser : true,
