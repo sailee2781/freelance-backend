96d97e5b686d747f383cf2b7a91802c565f5d3e2
Sailee Kadam <sailee.kadam19@vit.edu>
2022-07-03 00:55:50 +0530

    test: use local db for hosted backend

--- app.js
+++ app.js
@@ -6,6 +6,21 @@ var cors = require('cors')
 
 const app = express();
 app.use(cors())
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  )
+  if (req.method === "OPTIONS") {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "POST, PUT, PATCH, GET, DELETE"
+    )
+    return res.status(200).json({})
+  }
+  next()
+})
 const port = process.env.PORT || 5555;
 const router = require("./routers/topfreelancers")
 const router1 = require("./routers/prog")
@@ -15,10 +30,10 @@ app.use(router);
 app.use(router1);
 app.use(router2);
 
-app.get("/",async (req,res) => {
+app.get("/", async (req, res) => {
   res.send("Hello World")
 })
 
-app.listen(port , () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(port, () => {
+  console.log(`App running at PORT: ${port}`)
 })
