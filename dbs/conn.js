a151d322d9bbb33361342ce298ae08b8721283de
Sailee Kadam <sailee.kadam19@vit.edu>
2022-10-08 00:31:55 +0530

    prod ready backend

--- dbs/conn.js
+++ dbs/conn.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-const local = "mongodb://localhost:27017/freelance";
-const hosted = "mongodb+srv://freelance_services:freelance_services@cluster0.gyndj.mongodb.net/freelance-db?retryWrites=true&w=majority";
+// const db = "mongodb://localhost:27017/freelance";
+const db = "mongodb+srv://freelance_services:freelance_services@cluster0.gyndj.mongodb.net/freelance-db?retryWrites=true&w=majority";
 
 try {
-    mongoose.connect(local, { useNewUrlParser: true, useUnifiedTopology: true }, () =>
+    mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true }, () =>
         console.log("Database connected"));
 }
 catch (error) {
